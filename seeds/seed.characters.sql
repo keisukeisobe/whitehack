f89bd72e0d34a1bdf5ce3b1434faa496622b4a58
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-06-10 21:30:06 -0400

    added proper migrations and seed file for character

--- seeds/seed.characters.sql
+++ seeds/seed.characters.sql
@@ -1,4 +1,35 @@
-INSERT INTO characters (name)
+BEGIN;
+
+TRUNCATE
+  characters
+  RESTART IDENTITY CASCADE;
+
+INSERT INTO characters (characterName, strength, dexterity, constitution, intelligence, charisma, wisdom, class, characterLevel, hitDie, experience, attackValue, savingThrow, slots, miracles, groups, raises, gold, encumbrance, movement, armorClass, initiativeBonus, languages)
 VALUES
-  ('Valtyra')
-;
+  (
+    'Valtyra',
+    10,
+    10,
+    10,
+    10,
+    10,
+    10,
+    'Strong',
+    1,
+    1,
+    0,
+    10,
+    7,
+    1,
+    '',
+    '',
+    0,
+    '',
+    0,
+    30,
+    0,
+    0,
+    'Common'
+  );
+
+COMMIT;
