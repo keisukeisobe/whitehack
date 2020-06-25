2a44d4b6a4a90edc34b388d69b7b4caae6ad2e32
Victor Ng <vng@brandeis.edu>
2020-06-25 14:54:04 -0500

    equipment routing

---
+++ src/equipment/equipmentRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const EquipmentService = require("./equipmentService.js");
+const equipmentRouter = express.Router();
+const { requireAuth } = require("../middleware/jwt-auth");
+const jsonParser = express.json();
+
+//for getting all equipment
+equipmentRouter
+  .route("/equipment")
+  //uncomment to add authentication
+  //.all(requireAuth)
+  .get(async (req, res, next) => {
+    try {
+      const db = req.app.get("db");
+      let equipment = await EquipmentService.getAllEquipment(db);
+      res.json(equipment);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+module.exports = equipmentRouter;
