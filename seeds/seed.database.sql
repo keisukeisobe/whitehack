43c3a412f30ae013af10fbb4f2c5043c8d73f1a1
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-06-26 17:52:36 -0400

    fixed seed file hashed passwords

--- seeds/seed.database.sql
+++ seeds/seed.database.sql
@@ -39,8 +39,8 @@ VALUES
 
 INSERT INTO login(username, password, email)
 VALUES
-  ('kitsune', 'pbkdf2:sha256:150000$OyVhjZMd$6d892625b2b0cd896028c815d7e794ef04cc43a267833fedab9ac470fbcb2f24', 'vng@brandeis.edu'),
-  ('keisuke', 'pbkdf2:sha256:150000$XpK9QiuT$967021aeb59d4188378660755c6d4d531a6cde2e5f9d12768102c3b6bb3bcbf7', 'keisuke.isobe.2011@gmail.com');
+  ('kitsune', '$2a$12$I1V21ZUYqqH2q5208w2DsuYv0FeBEJ/BLnPgCJavXyIQyn3QuA4qq', 'vng@brandeis.edu'),
+  ('keisuke', '$2a$12$I1V21ZUYqqH2q5208w2DsuYv0FeBEJ/BLnPgCJavXyIQyn3QuA4qq', 'keisuke.isobe.2011@gmail.com');
 
 INSERT INTO userchars(user_id, character_id)
 VALUES
