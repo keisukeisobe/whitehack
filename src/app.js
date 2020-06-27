4da9fc9489fe3a5f0613bce45fe0a8e1b00bf853
Victor Ng <vng@brandeis.edu>
2020-06-26 21:53:45 -0500

    post route for equipment created

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const characterRouter = require('./characters/characterRouter');
-const usersRouter = require('./users/usersRouter');
-const authRouter = require('./auth/authRouter');
-const equipmentRouter = require('./equipment/equipmentRouter');
-const itemsRouter = require('./items/itemsRouter');
-const weaponsRouter = require('./weapons/weaponsRouter');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const characterRouter = require("./characters/characterRouter");
+const usersRouter = require("./users/usersRouter");
+const authRouter = require("./auth/authRouter");
+const equipmentRouter = require("./equipment/equipmentRouter");
+const itemsRouter = require("./items/itemsRouter");
+const weaponsRouter = require("./weapons/weaponsRouter");
 
 const app = express();
 
-const morganOption = (process.env.NODE_ENV === 'production') ? 'tiny' : 'common';
+const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
@@ -27,14 +27,14 @@ app.get("/ponies", function (req, res) {
   res.send("Ponies!");
 });
 
-// app.use("/api", [authRouter, usersRouter, characterRouter, equipmentRouter]);
-
-app.use('/api', authRouter);
-app.use('/api', usersRouter);
-app.use('/api', characterRouter);
-app.use('/api', equipmentRouter);
-app.use('/api', weaponsRouter);
-app.use('/api', itemsRouter);
+app.use("/api", [
+  authRouter,
+  usersRouter,
+  characterRouter,
+  equipmentRouter,
+  weaponsRouter,
+  itemsRouter,
+]);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
--- src/equipment/equipmentRouter.js
+++ src/equipment/equipmentRouter.js
@@ -1,22 +1,65 @@
 const express = require("express");
-const EquipmentService = require('./equipmentService.js');
+const EquipmentService = require("./equipmentService.js");
 const equipmentRouter = express.Router();
 const { requireAuth } = require("../middleware/jwt-auth");
 const jsonParser = express.json();
 
 //for getting all equipment
 equipmentRouter
-  .route('/equipment')
+  .route("/equipment")
   //uncomment to add authentication
   //.all(requireAuth)
   .get(async (req, res, next) => {
     try {
-      const db = req.app.get('db');
+      const db = req.app.get("db");
       let equipment = await EquipmentService.getAllEquipment(db);
       res.json(equipment);
     } catch (err) {
       next(err);
     }
+  })
+
+  .post(jsonParser, (req, res, next) => {
+    const {
+      id,
+      name,
+      armorclass,
+      cost,
+      weight,
+      special,
+      description,
+    } = req.body;
+    //class is protected keyword? cannot use in newCharacter object constructor
+    const newEquipment = {
+      id,
+      name,
+      armorclass,
+      cost,
+      weight,
+      special,
+      description,
+    };
+    const requiredFields = [
+      "name",
+      "armorclass",
+      "cost",
+      "weight",
+      "description",
+    ];
+    //error validation
+    for (const key of requiredFields) {
+      if (!(key in req.body)) {
+        return res
+          .status(400)
+          .json({ error: `Missing ${key} in request body` });
+      }
+    }
+    const db = req.app.get("db");
+    EquipmentService.insertEquipment(db, newEquipment)
+      .then((newEquipment) => {
+        res.json(newEquipment);
+      })
+      .catch(next);
   });
 
 module.exports = equipmentRouter;
