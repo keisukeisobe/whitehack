711a189c0478fab273a2df9da4d228062e4d4dc6
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-07-29 11:40:44 -0400

    changed database local

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://kitsundere@localhost/whitehack",
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/whitehack",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
 };
