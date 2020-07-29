ff4d432c2ebd509b8886ab91783c2ee34d8b3599
Victor Ng <vng@brandeis.edu>
2020-07-29 10:23:02 -0500

    added individual equip call

--- src/equipment/equipmentService.js
+++ src/equipment/equipmentService.js
@@ -2,6 +2,9 @@ const EquipmentService = {
   getAllEquipment(db) {
     return db.from('equipment').select('*');
   },
+  getEquipmentId(db, itemid){
+    //return db.from('equipment').select('*').where('id');
+  },
   insertEquipment(db, newEquipment) {
     return db.insert(newEquipment).into('equipment').returning('*');
   },
